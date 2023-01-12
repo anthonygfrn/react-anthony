9a778d79622ec984941085d4b53bee742001151a
Anthony Gufron <anthonygfrn@gmail.com>
2023-01-12 20:58:33 +0700

    Init Project

--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -1,10 +1,14 @@
 export const aboutMeData = [
-	{
-		id: 1,
-		bio: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.',
-	},
-	{
-		id: 2,
-		bio: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?',
-	},
+  {
+    id: 1,
+    bio: "To prepare for my professional career, I earned my bachelor’s degree from Universiti Teknologi Malaysia and completed an internship at Kumparan as a Front-End Engineer  to gain the field experience needed to begin my career. I have theoretical application knowledge and the ability to help develop new technology.",
+  },
+  {
+    id: 2,
+    bio: "My technical abilities include programming knowledge, data structure, and algorithms. I have multiple languages that I am familiar and I have experience with which includes Javascript, Python, and Java. As my primary experience is as a front-end I am familiar with many front-end stacks primarily React framework. Other than that, I am also familiar with backend development, mobile development, DevOps, data analytics, UI/UX,  and data science.",
+  },
+  {
+    id: 3,
+    bio: "I believe that my positive attitude and great communication skills could help me stand out from other engineers. I believe in teamwork and have the ability to either follow or lead a team to ensure that projects are completed competently. ",
+  },
 ];
